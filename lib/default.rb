7494d9ac322ed5d44fdd5246e45b45b064ae7b8a
Anthony SIMONET <anthony.simonet@rutgers.edu>
2018-02-21 10:34:55 -0500

    Updates site title for Rutgers

--- lib/default.rb
+++ lib/default.rb
@@ -5,7 +5,7 @@ require 'nanoc/data_sources/bibtex_data_source'
 
 def description
 	if @item[:description].nil?
-		'Personal webpage of Anthony SIMONET, post-doctoral researcher at Inria.'
+		'Personal webpage of Anthony SIMONET, Post-Doctoral Associate at Rutgers University.'
 	else
 		@item[:description]
 	end
